abe87aa73e006d41f906853f38cc219b38e49e41
Bashari Rivka <basrivka1@gmail.com>
2025-06-03 00:16:01 +0300

    Add button in cart list

--- src/pages/CartList.jsx
+++ src/pages/CartList.jsx
@@ -3,13 +3,15 @@ import { loadCart } from '../store/actions/product.action.js'
 
 import { CartPreview } from '../cmps/CartPreview.jsx'
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 
 export function CartList() {
 
-    const [filterBy, setFilterBy] = useState({ quantity:true });
+    const [filterBy, setFilterBy] = useState({ quantity: true });
     const cartProducts = useSelector(storeState => storeState.productModule.cart)
     // const currentProduct = Products.length > 0 ? Products[Products.length - 1] : null;
+    const navigate = useNavigate()
 
     useEffect(() => {
         loadCart()
@@ -18,6 +20,8 @@ export function CartList() {
     return (
         <section className='my-cart'>
             <h1 >MY CART</h1>
+            <button onClick={() => navigate('/')}>Back to Home</button>
+
             <ul className="cart-list">
                 {cartProducts?.map(product => (
                     <li className="cart-preview" key={product._id}>
